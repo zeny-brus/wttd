6d6be19f871325f551552e6894b0d4f5c02d3f0e
zamenis <zamenis@pop-os.localdomain>
2023-08-09 21:00:48 -0300

    realiza inscricao

--- eventex/settings.py
+++ eventex/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'UTC'
 
@@ -122,3 +122,12 @@ STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 
@@ -9,7 +10,7 @@ class SubscribeTest(TestCase):
         self.assertEqual(200, self.response.status_code)
     
     def test_template(self):
-        """deve usar o template subscription/subscription_form.html"""
+        """deve usar o template subscriptions/subscription_form.html"""
         self.assertTemplateUsed(self.response, 'subscriptions/subscription_form.html')
     
     def test_html(self):
@@ -33,4 +34,70 @@ class SubscribeTest(TestCase):
         """o forms deve ter 4 campos"""
         form = self.response.context['form']
         self.assertEqual(['name', 'cpf','email','phone'], list(form.fields))
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = dict(name='Zeny Brus', cpf='12345678901',
+                    email='zeny-brus@email.com.br', phone='55-99988-7766')
+        self.response = self.client.post('/inscricao/', data) 
+                
+   
+    def test_post(self):
+        """POST válido devera redirecionar para o caminho /inscricao/"""
+        self.assertEqual(302, self.response.status_code)
+    
+    def test_send_subscribe_email(self):
+        self.assertEqual(1,len(mail.outbox))
+    
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        expect = 'confirmação de inscrição'
+
+        self.assertEqual(expect, email.subject)
+    
+    def test_subscription_email_from(self):
+        email = mail.outbox[0]
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, email.from_email)
+    
+    def test_subscription_email_to(self):
+        email = mail.outbox[0]
+        expect = ['contato@eventex.com.br','zeny-brus@email.com.br']
+
+        self.assertEqual(expect, email.to)
+    
+    def test_subscription_email_body(self):
+        email = mail.outbox[0]
+        
+        self.assertIn('Zeny Brus', email.body)
+        self.assertIn('12345678901', email.body)
+        self.assertIn('zeny-brus@email.com.br', email.body)
+        self.assertIn('55-99988-7766', email.body)
+
+class SubscribeInvalidPost(TestCase):
+    def setUp(self):
+        self.response = self.client.post('/inscricao/', {})
+        
+    def test_post(self):
+        """POST invalido não deverá redirecionar"""        
+        self.assertEqual(200, self.response.status_code)
+    
+    def test_template(self):
+        self.assertTemplateUsed(self.response,'subscriptions/subscription_form.html')
+    
+    def test_has_form(self):
+        form = self.response.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+    
+    def test_form_has_errors(self):
+        form = self.response.context['form']
+        self.assertTrue(form.errors)
+    
+class SubscribeSuccessMessage(TestCase):
+    def test_message(self):
+        data = dict(name='zeny brus', cpf='12345678901',
+                    email='zeny-brus@email.com', phone='51-98877-6655')
+        response = self.client.post('/inscricao/',data,follow=True)
+        self.assertContains(response, 'Inscricao realizada com sucesso!')
 
